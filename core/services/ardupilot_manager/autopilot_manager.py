7a4619c32b85c5a93b0d1e7da3f41632e3ee89d1
RadAlpaca11 <mayahayden@icloud.com>
2025-09-10 11:30:10 -0700

    fixing again

--- core/services/ardupilot_manager/autopilot_manager.py
+++ core/services/ardupilot_manager/autopilot_manager.py
@@ -10,6 +10,8 @@ import psutil
 from commonwealth.mavlink_comm.VehicleManager import VehicleManager
 from commonwealth.utils.Singleton import Singleton
 from elftools.elf.elffile import ELFFile
+from loguru import logger
+
 from exceptions import (
     AutoPilotProcessKillFail,
     NoDefaultFirmwareAvailable,
@@ -18,7 +20,6 @@ from exceptions import (
 from firmware.FirmwareManagement import FirmwareManager
 from flight_controller_detector.Detector import Detector as BoardDetector
 from flight_controller_detector.linux.linux_boards import LinuxFlightController
-from loguru import logger
 from mavlink_proxy.Endpoint import Endpoint, EndpointType
 from mavlink_proxy.exceptions import EndpointAlreadyExists
 from mavlink_proxy.Manager import Manager as MavlinkManager
