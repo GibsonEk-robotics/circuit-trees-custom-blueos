5a5bbaaa3c36043f86b06d43235c555ac79726a6
RadAlpaca11 <mayahayden@icloud.com>
2025-09-10 11:22:18 -0700

    idek

--- core/services/ardupilot_manager/autopilot_manager.py
+++ core/services/ardupilot_manager/autopilot_manager.py
@@ -591,7 +591,7 @@ class AutoPilotManager(metaclass=Singleton):
             try:
                 from pydub import AudioSegment
                 from pydub.playback import play
-                
+
                 audio = AudioSegment.from_mp3("/usr/bin/audio/beeps.mp3")
             except Exception as e:
                 print(f"Could not play audio: {e}")
