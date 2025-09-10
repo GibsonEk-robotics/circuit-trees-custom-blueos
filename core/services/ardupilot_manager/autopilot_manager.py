311f1547d7bec83144ef3a0ca04cebab8747de76
RadAlpaca11 <mayahayden@icloud.com>
2025-09-10 11:08:46 -0700

    trying isort

--- core/services/ardupilot_manager/autopilot_manager.py
+++ core/services/ardupilot_manager/autopilot_manager.py
@@ -1,18 +1,16 @@
 import asyncio
 import os
 import pathlib
-import pydub
 import subprocess
 import time
 from copy import deepcopy
 from typing import Any, List, Optional, Set
 
 import psutil
+import pydub
 from commonwealth.mavlink_comm.VehicleManager import VehicleManager
 from commonwealth.utils.Singleton import Singleton
 from elftools.elf.elffile import ELFFile
-from loguru import logger
-
 from exceptions import (
     AutoPilotProcessKillFail,
     NoDefaultFirmwareAvailable,
@@ -21,6 +19,7 @@ from exceptions import (
 from firmware.FirmwareManagement import FirmwareManager
 from flight_controller_detector.Detector import Detector as BoardDetector
 from flight_controller_detector.linux.linux_boards import LinuxFlightController
+from loguru import logger
 from mavlink_proxy.Endpoint import Endpoint, EndpointType
 from mavlink_proxy.exceptions import EndpointAlreadyExists
 from mavlink_proxy.Manager import Manager as MavlinkManager
